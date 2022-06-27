8ad03089b827f54cbf50068d8e476512b0c6ad0d
Shivani <shivani@dhatripatra.org>
2022-06-27 19:11:48 +0530

    First Commit

---
+++ team1.php
@@ -0,0 +1,403 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php include 'assets/include/analytics.php';?>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="ha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>   
+  <!-- Required meta tags -->
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">  
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Dhatri Patra| About</title>
+    <!-- web fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Karla:400,700&amp;display=swap" rel="stylesheet">
+    <!-- //web fonts -->
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-freedom.css">
+  </head>
+  <body>
+<script src='../../../../../../../ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="../../../../../../../m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('flexbar', 'CKYI627U', 'placement:vgbcsayoutscom');
+  	}
+})();
+</script>
+<script>
+(function(){
+if(typeof _bsa !== 'undefined' && _bsa) {
+	// format, zoneKey, segment:value, options
+	_bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+}
+})();
+</script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('stickybox', 'CKYI653J', 'placement:vgbcsayoutscom');
+  	}
+})();
+</script>
+<!--<script>(function(v,d,o,ai){ai=d.createElement("script");ai.defer=true;ai.async=true;ai.src=v.location.protocol+o;d.head.appendChild(ai);})(window, document, "//a.vdo.ai/core/vgbcsayouts_V2/vdo.ai.js?vdo=34");</script>-->
+<div id="codefund"><!-- fallback content --></div>
+<script src="../../../../../../../codefund.io/properties/441/funder.js" async="async"></script>
+
+<!-- Global site tag (gtag.js) - Google Analytics Removed -->
+<script async src='../../../../../../js/autotrack.js'></script>
+
+<meta name="robots" content="noindex">
+<body>
+	<!-- Demo bar start -->
+  <link rel="stylesheet" href="../../../../../../assests/css/font-awesome.min.css">
+<!-- New toolbar-->
+<style>
+* {
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+}
+
+
+#vgbcsDemoBar.vgbcs-demo-bar {
+  top: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 9999;
+  padding: 40px 5px;
+  padding-top:70px;
+  margin-bottom: 70px;
+  background: #0D1326;
+  border-top-left-radius: 9px;
+  border-bottom-left-radius: 9px;
+}
+
+#vgbcsDemoBar.vgbcs-demo-bar a {
+  display: block;
+  color: #e6ebff;
+  text-decoration: none;
+  line-height: 24px;
+  opacity: .6;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+#vgbcsDemoBar.vgbcs-demo-bar span.vgbcs-icon {
+  display: block;
+}
+
+#vgbcsDemoBar.vgbcs-demo-bar a:hover {
+  opacity: 1;
+}
+
+#vgbcsDemoBar.vgbcs-demo-bar .vgbcs-icon svg {
+  color: #e6ebff;
+}
+#vgbcsDemoBar.vgbcs-demo-bar .responsive-icons {
+  margin-top: 30px;
+  border-top: 1px solid #41414d;
+  padding-top: 40px;
+}
+#vgbcsDemoBar.vgbcs-demo-bar .demo-btns {
+  border-top: 1px solid #41414d;
+  padding-top: 30px;
+}
+#vgbcsDemoBar.vgbcs-demo-bar .responsive-icons a span.fa {
+  font-size: 26px;
+}
+#vgbcsDemoBar.vgbcs-demo-bar .no-margin-bottom{
+  margin-bottom:0;
+}
+.toggle-right-sidebar span {
+  background: #0D1326;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: #e6ebff;
+  border-radius: 50px;
+  font-size: 26px;
+  cursor: pointer;
+  opacity: .5;
+}
+.pull-right {
+  float: right;
+  position: fixed;
+  right: 0px;
+  top: 70px;
+  width: 90px;
+  z-index: 99999;
+  text-align: center;
+}
+/* ============================================================
+RIGHT SIDEBAR SECTION
+============================================================ */
+
+#right-sidebar {
+  width: 90px;
+  position: fixed;
+  height: 100%;
+  z-index: 1000;
+  right: 0px;
+  top: 0;
+  margin-top: 60px;
+  -webkit-transition: all .5s ease-in-out;
+  -moz-transition: all .5s ease-in-out;
+  -o-transition: all .5s ease-in-out;
+  transition: all .5s ease-in-out;
+  overflow-y: auto;
+}
+
+/* ============================================================
+RIGHT SIDEBAR TOGGLE SECTION
+============================================================ */
+
+.hide-right-bar-notifications {
+  margin-right: -300px !important;
+  -webkit-transition: all .3s ease-in-out;
+  -moz-transition: all .3s ease-in-out;
+  -o-transition: all .3s ease-in-out;
+  transition: all .3s ease-in-out;
+}
+
+
+
+@media (max-width: 992px) {
+  #vgbcsDemoBar.vgbcs-demo-bar a.desktop-mode{
+      display: none;
+
+  }
+}
+@media (max-width: 767px) {
+  #vgbcsDemoBar.vgbcs-demo-bar a.tablet-mode{
+      display: none;
+
+  }
+}
+@media (max-width: 568px) {
+  #vgbcsDemoBar.vgbcs-demo-bar a.mobile-mode{
+      display: none;
+  }
+  #vgbcsDemoBar.vgbcs-demo-bar .responsive-icons {
+      margin-top: 0px;
+      border-top: none;
+      padding-top: 0px;
+  }
+  #right-sidebar,.pull-right {
+      width: 90px;
+  }
+  #vgbcsDemoBar.vgbcs-demo-bar .no-margin-bottom-mobile{
+      margin-bottom: 0;
+  }
+}
+</style>
+<!--<div class="pull-right toggle-right-sidebar">
+<span class="fa title-open-right-sidebar tooltipstered fa-angle-double-right"></span>
+</div>-->
+
+<!--<div id="right-sidebar" class="right-sidebar-notifcations nav-collapse">
+<div class="bs-example bs-example-tabs right-sidebar-tab-notification" data-example-id="togglable-tabs">
+
+    <div id="vgbcsDemoBar" class="vgbcs-demo-bar">
+        <div class="demo-btns">
+        <a href="https://vgbcsayouts.com/?p=41702">
+            <span class="vgbcs-icon -back">
+                <span class="fa fa-arrow-left"></span>
+            </span>
+            <span class="vgbcs-text">Back</span>
+        </a>
+        <a href="https://vgbcsayouts.com/?p=41702">
+            <span class="vgbcs-icon -download">
+                <span class="fa fa-download"></span>
+            </span>
+            <span class="vgbcs-text">Download</span>
+        </a>
+        <a href="https://vgbcsayouts.com/checkout/?add-to-cart=41702" class="no-margin-bottom-mobile">
+            <span class="vgbcs-icon -buy">
+                <span class="fa fa-shopping-cart"></span>
+            </span>
+            <span class="vgbcs-text">Buy</span>
+        </a>
+    </div>
+        <div class="responsive-icons">
+            <a href="#url" class="desktop-mode">
+                <span class="vgbcs-icon -desktop">
+                    <span class="fa fa-desktop"></span>
+                </span>
+            </a>
+            <a href="#url" class="tablet-mode">
+                <span class="vgbcs-icon -tablet">
+                    <span class="fa fa-tablet"></span>
+                </span>
+            </a>
+            <a href="#url" class="mobile-mode no-margin-bottom">
+                <span class="vgbcs-icon -mobile">
+                    <span class="fa fa-mobile"></span>
+                </span>
+            </a>
+        </div>
+    </div>
+    <div class="right-sidebar-panel-content animated fadeInRight" tabindex="5003"
+        style="overflow: hidden; outline: none;">
+    </div>
+</div>
+</div>
+</div>-->
+
+
+<!-- Top Menu 1 -->
+<?php include 'assets/include/navbar.php';?>
+
+<section class="vgbcs-about-breadcrum1">
+  <div class="breadcrum-bg1 py-sm-5 py-4">
+    <div class="container py-lg-3">
+      <h2>About Us</h2>
+      <p><a href="index.php">Home</a> &nbsp; / &nbsp; About</p>
+    </div>
+  </div>
+</section>
+<!---728x90--->
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Our Team, Our Helping Han​​​ds">
+    <meta name="description" content="">
+    <title>Home</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="team1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.13.4, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    
+    
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": ""
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-xl-mode">
+    <section class="u-clearfix u-section-1" id="sec-dd67">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-hover-feature u-text u-text-1">Our Team<br>
+        </h1>
+      </div>
+      
+    </section>
+    <section class="u-clearfix u-section-2" id="sec-13e8">
+      <div class="u-clearfix u-sheet u-valign-middle-lg u-sheet-1">
+        <div class="u-expanded-width u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <div alt="" class="u-image u-image-circle u-image-1" src="" data-image-width="435" data-image-height="435"></div>
+                <h3 class="u-align-center u-text u-text-1"> Mr Raghavendra M</h3>
+                <p class="u-align-center u-text u-text-2"> President</p>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-2">
+                <div alt="" class="u-image u-image-circle u-image-2" src="" data-image-width="434" data-image-height="435"></div>
+                <h3 class="u-align-center u-text u-text-3"> Mr. Ujwal Reddy K S</h3>
+                <p class="u-align-center u-text u-text-4"> Vice President</p>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-3">
+                <div alt="" class="u-image u-image-circle u-image-3" src="" data-image-width="435" data-image-height="436"></div>
+                <h3 class="u-align-center u-text u-text-5"> Ms. Maria Sneha</h3>
+                <p class="u-align-center u-text u-text-6"> Secretary and Head of Volunteers</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-3" id="sec-13d5">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-expanded-width u-layout-grid u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <div alt="" class="u-image u-image-circle u-image-1" src="" data-image-width="434" data-image-height="435"></div>
+                <h3 class="u-align-center u-text u-text-1"> Mr. Hari Prasad</h3>
+                <p class="u-align-center u-text u-text-2"> Head of Public Communications</p>
+              </div>
+            </div>
+            <div class="u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-2">
+                <div alt="" class="u-image u-image-circle u-image-2" src="" data-image-width="435" data-image-height="436"></div>
+                <h3 class="u-align-center u-text u-text-3"> Ms. Deepshika Sharadha</h3>
+                <p class="u-align-center u-text u-text-4"> Treasurer</p>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-3">
+                <div alt="" class="u-image u-image-circle u-image-3" src="" data-image-width="434" data-image-height="436"></div>
+                <h3 class="u-align-center u-text u-text-5"> Mr. Pramod</h3>
+                <p class="u-align-center u-text u-text-6"> Vice-Secretary</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-4" id="sec-c75c">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-expanded-width-sm u-expanded-width-xs u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <h3 class="u-align-center u-text u-text-1"> Mr. Varun Govind</h3>
+                <p class="u-align-center u-text u-text-2"> Vice-Secretary</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div alt="" class="u-image u-image-circle u-image-1" src="" data-image-width="426" data-image-height="424"></div>
+      </div>
+    </section>
+    <section class="u-clearfix u-section-5" id="sec-dcb0">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-text u-text-default u-text-1">Our Helping Hands<span style="font-style: italic;">
+            <span style="font-style: normal;">
+              <span style="font-weight: 700;"></span>
+            </span>
+          </span>
+        </h2>
+        <div class="u-expanded-width u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-1">
+                <div alt="" class="u-image u-image-circle u-image-1" src="" data-image-width="426" data-image-height="424"></div>
+                <h3 class="u-align-center u-text u-text-2"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Ms. Sherley Gracia</h3>
+                <p class="u-align-center u-text u-text-3">Technical</p>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-repeater-item">
+              <div class="u-container-layout u-similar-container u-container-layout-2">
+                <div alt="" class="u-image u-image-circle u-image-2" src="" data-image-width="426" data-image-height="424"></div>
+                <h3 class="u-align-center u-text u-text-4">&nbsp; &nbsp; &nbsp; &nbsp; ​&nbsp; &nbsp;Mr. Karan Kadhka</h3>
+                <p class="u-align-center u-text u-text-5">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Social Activities</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <?php include 'assets/include/footer.php';?>
+  
+</body></html>
